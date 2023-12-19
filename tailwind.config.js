26b6c41ba8f16cb2ebf1f822d85faeb0b727da7f
RajatSharma <withrajatsharma@gmail.com>
2023-12-19 22:49:34 +0530

    section 1 completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "light":"rgba(246, 246, 246, 1)",
+        "dark":"rgba(41, 37, 38, 1)",
+        "light-sec":"rgba(224, 234, 243, 1)",
+        "pri-ora":"rgba(248, 102, 66, 1)",
+      },
+      fontFamily:{
+        "monts":["Montserrat", "sans-serif"],
+      },
+    
+    },
   },
   plugins: [],
 }
