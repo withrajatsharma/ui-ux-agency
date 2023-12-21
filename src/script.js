c577af32d61bb99ccbce0a9f00d380ab6ce92e71
RajatSharma <withrajatsharma@gmail.com>
2023-12-21 23:09:23 +0530

    add locomotion and some updates

--- src/script.js
+++ src/script.js
@@ -9,3 +9,21 @@
         },
       });
 })();
+
+
+(()=>{
+  const lenis = new Lenis({
+    duration: 2.5, // increase or decrese duration value to change smoothness values on website
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+            
+            
+    });
+    function raf(time) {
+     lenis.raf(time);
+     //ScrollTrigger.update(); //only apply when using scroll trigger on website
+      requestAnimationFrame(raf);
+    }
+            
+    requestAnimationFrame(raf);
+})();
